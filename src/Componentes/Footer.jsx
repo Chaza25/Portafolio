127df584771eacdb9fb9312332d344382349b622
Agustin Chazarreta Cruz <aguschacruz@gmail.com>
2025-04-28 19:42:32 -0300

    Arreglando Footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -2,11 +2,9 @@ import "./Footer.css"
 
 const PiePagina = ({otros}) => {
     return(
-        <>
         <div className="Foot">
             <p>Numero de cel: {otros}</p>
         </div>
-        </>
     )
 }
 
