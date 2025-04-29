25a4785910445229593077989c2ec21ca35c62fa
Agustin Chazarreta Cruz <aguschacruz@gmail.com>
2025-04-29 00:31:56 -0300

    Mejoras

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -3,7 +3,10 @@ import "./Footer.css"
 const Footer = ({otros}) => {
     return(
         <div className="Foot">
-            <p>Numero de{otros}</p>
+            <p>Numero de {otros}</p>
+            <a href="https://github.com/Chaza25/" target="_blank">
+            <img src="./src/assets/github_dark.svg" alt="GitHub" />GitHub
+            </a>
         </div>
     )
 }
